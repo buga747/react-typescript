a700e52375d061b347ddf3a26635b401cee30201
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-07-31 20:39:58 +0300

    add special types - union, react.node

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Greet from './components/Greet'
+import Heading from './components/Heading'
+import Oscar from './components/Oscar'
 import Person from './components/Person'
 import PersonList from './components/PersonList'
 import Status from './components/Status'
@@ -28,10 +30,14 @@ function App() {
 
   return (
     <>
-      <Greet name='Oleh' messageCount={10} isLoggedIn={false} />
+      <Greet name='Oleh'  isLoggedIn={true} />
       <Person name={personName} />
       <PersonList names={nameList} />
       <Status status='loading' />
+      <Heading> My new text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo di Caprio</Heading>
+      </Oscar>
     </>
   )
 }
